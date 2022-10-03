30c519272a412605b3c105f83b5b8363a67e0618
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 19:51:44 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
-//W pliku Main07.java napisz publiczną metodę checkEvenOdd, która:
-//przyjmie parametr liczbowy number,
-//będzie zwracać wartość typu String,
-//zwróci wynik "even", jeśli liczba jest parzysta,
-//zwróci wynik "odd", jeśli liczba jest nieparzysta.
+//W pliku Main08.java napisz publiczną metodę maxOfThree,
+// która przyjmie trzy parametry liczbowe. Metoda ma zwrócić największą liczbę.
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Number:");
+        System.out.println("Number 1:");
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        String result = checkEvenOdd(number);
+        int num1 = scanner.nextInt();
+        System.out.println("Number 2:");
+        Scanner scanner2 = new Scanner(System.in);
+        int num2 = scanner2.nextInt();
+        System.out.println("Number 3:");
+        Scanner scanner3 = new Scanner(System.in);
+        int num3 = scanner3.nextInt();
+        int result = maxOfThree(num1, num2, num3);
         System.out.println("Number is:");
         System.out.println(result);
     }
-    public static String checkEvenOdd(int number){
-        if(number%2 == 0){
-            return "even";
-        } else {
-            return "odd";
-        }
+    public static int maxOfThree(int num1,int num2,int num3){
+        int max= Math.max(num1,num2);
+        return Math.max(max,num3);
 
     }
 }
