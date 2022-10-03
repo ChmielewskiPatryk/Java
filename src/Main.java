f76b46b8c2078610d11e17543c0ae8e27a6b3a2b
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 19:37:51 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
-//W pliku Main05.java napisz publiczną metodę calculateNetto, która przyjmie argumenty:
-//gross: kwotę brutto ceny zakupu,
-//vat: wartość podatku VAT. Możesz założyć, że VAT ma być liczbą zmiennoprzecinkową z zakresu 0 – 1.
-//Metoda ma zwrócić wartość netto ceny. Napisz w komentarzu, jakie obliczenia musisz wykonać.
+//W pliku Main06.java napisz publiczną metodę checkMaturity, która:
+//przyjmie parametr liczbowy age oznaczający wiek użytkownika,
+//sprawdzi, czy użytkownik jest pełnoletni,
+//zwróci wartość true – jeśli jest pełnoletni,
+//zwróci wartość false – jeśli nie jest.
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gross:");
+        System.out.println("Age:");
         Scanner scanner = new Scanner(System.in);
-        double gross = scanner.nextDouble();
-        System.out.println("Vat:");
-        Scanner scanner1 = new Scanner(System.in);
-        double vat = scanner1.nextDouble();
-        double result = calculateNetto(gross,vat);
-        System.out.println("Netto:");
+        int age = scanner.nextInt();
+        boolean result = checkMaturity(age);
+        System.out.println("Mature:");
         System.out.println(result);
     }
-    public static double calculateNetto(double gross, double vat){
-        //gross = net + net*vat
-        //gross = net(1+ vat)
-        //net = gross/(1+vat)
+    public static boolean checkMaturity(int age){
+        if(age>=18){
+            return true;
+        } else {
+            return false;
+        }
 
-        return (gross/(1+vat));
     }
 }
