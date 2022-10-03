3c3cc2e3d058baea6220b61603498c61ffb22a83
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 22:57:40 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 //stwórz tablicę o nazwie numbers zawierającą 10 dowolnych liczb,
-//posortuj rosnąco tablicę wykorzystując metody klasy Arrays,
-//wyświetl w konsoli posortowaną tablicę – również przy pomocy metody z klasy Arrays.
-
+//stwórz drugą tablicę, nazwij ją secondNumbers, wypełnij 10 dowolnymi liczbami,
+//stwórz trzecią tablicę zawierającą sumy elementów, które mają w tablicach pozycję o tym samym indeksie.
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {2, 5, 7, 3, 5, 2, 8, 6, 4, 9};
-        Arrays.sort(numbers);
-        for (int num : numbers) {
-            System.out.print(num + ",");
+        int[] secondNumbers = {6, 8, 3, 9, 5, 4, 2, 1, 4, 5};
+        int[] thirdNumbers = new int[10];
+        // dziala ale nie wiem czy tu chca jakies Array methods?
+        for (int i =0; i<10;i++){
+            thirdNumbers[i]=numbers[i]+secondNumbers[i];
+            System.out.print(thirdNumbers[i]+",");
         }
-
     }
 }
