cb47f4097d5dae7aff4a83d711fd8c32eea5f3e7
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 19:15:34 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
-//W pliku Main03.java napisz publiczną metodę convertToUsd,
-//która przyjmuje parametr pln, czyli kwotę w złotówkach.
-//Metoda ma zwrócić podaną kwotę w dolarach amerykańskich.
-//Jako przelicznik przyjmij wartość 4.04 PLN = 1 USD.
+//W pliku Main04.java napisz publiczną metodę createName, która przyjmie następujące parametry:
+//name: imię,
+//surname: nazwisko,
+//nickname: pseudonim.
+//Metoda ma zwrócić łańcuch tekstowy z połączonymi parametrami, w postaci: "imię pseudonim nazwisko".
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Type amount ZL to conver to USD:");
+        System.out.println("Name:");
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        double result = convertToUsd(number);
-        System.out.println("Square:");
+        String name = scanner.nextLine();
+        System.out.println("Surname:");
+        Scanner scanner1 = new Scanner(System.in);
+        String surname = scanner1.nextLine();
+        System.out.println("Nickname:");
+        Scanner scanner2 = new Scanner(System.in);
+        String nickname = scanner2.nextLine();
+        String result = createName(name,surname,nickname);
+        System.out.println("Joined string:");
         System.out.println(result);
     }
-    public static double convertToUsd(int num){
-        double ratio =4.04;
-        return num*ratio;
+    public static String createName(String name, String surname, String nickname){
+        return (name + " " + nickname + " " + surname);
     }
 }
