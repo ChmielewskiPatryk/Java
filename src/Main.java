84f363a9a9e38df270467cee9b1cc6fbda2bdc1d
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 22:48:39 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 //stwórz tablicę o nazwie numbers zawierającą 10 dowolnych liczb,
-//stwórz zmienną tablicową reverse i umieść w niej elementy tablicy numbers w odwrotnej kolejności,
-//wypisz na konsoli elementy z tablicy reverse.
+//posortuj rosnąco tablicę wykorzystując metody klasy Arrays,
+//wyświetl w konsoli posortowaną tablicę – również przy pomocy metody z klasy Arrays.
 
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-     int[] numbers = {2,5,7,3,5,2,8,6,4,9};
-     int[] reverse = new int[10];
-        // nie wiem czy o to chodzilo ale dziala
-     for (int i=0; i<10;i++){
-      reverse[i]=numbers[9-i];
-      System.out.print(reverse[i]+",");
-     }
-    }
+        int[] numbers = {2, 5, 7, 3, 5, 2, 8, 6, 4, 9};
+        Arrays.sort(numbers);
+        for (int num : numbers) {
+            System.out.print(num + ",");
+        }
 
+    }
 }
