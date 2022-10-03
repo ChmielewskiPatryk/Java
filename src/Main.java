1c7f571b0a4c9d1b6a7cf2e23d4c967b41f9d149
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 22:31:35 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,17 @@
-import java.util.Arrays;
+//stwórz tablicę o nazwie numbers zawierającą 10 dowolnych liczb,
+//stwórz zmienną tablicową reverse i umieść w niej elementy tablicy numbers w odwrotnej kolejności,
+//wypisz na konsoli elementy z tablicy reverse.
 
-//stwórz tablicę o nazwie randNumbers
-//przy użyciu pętli for wypełnij tablicę przez 20 losowych liczb z zakresu 0-100, (do otrzymania losowej wartości użyj sposobu opisanego w snippetach),
-//wypisz w konsoli minimalną wartość z tablicy.
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Array:");
-        int min = 100;       //rozwiazanie 1
-        int min2;            //rozwiazanie 2
-        int[] randNumbers = new int[20];
-        for(int i=0; i<randNumbers.length;i++){
-            randNumbers[i] = (int)(Math.random()*101);
-            min = Math.min(randNumbers[i],min);  //rozwiazanie 1
-        }
-        System.out.println(min);   //rozwiazanie 1
-
-
-
-        Arrays.sort(randNumbers); //rozwiazanie 2
-        System.out.println(randNumbers[0]); //rozwiazanie 2
-
+     int[] numbers = {2,5,7,3,5,2,8,6,4,9};
+     int[] reverse = new int[10];
+        // nie wiem czy o to chodzilo ale dziala
+     for (int i=0; i<10;i++){
+      reverse[i]=numbers[9-i];
+      System.out.print(reverse[i]+",");
+     }
     }
 
 }
