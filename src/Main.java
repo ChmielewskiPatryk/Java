52925127a22e6f2797c1b3fcdcee85ab8814fe23
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 19:00:52 +0200

    done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-//W pliku Main02.java napisz metodę o sygnaturze:
-//public static int square(int num),
-//która zwróci wartość num podniesioną do kwadratu.
+//W pliku Main03.java napisz publiczną metodę convertToUsd,
+//która przyjmuje parametr pln, czyli kwotę w złotówkach.
+//Metoda ma zwrócić podaną kwotę w dolarach amerykańskich.
+//Jako przelicznik przyjmij wartość 4.04 PLN = 1 USD.
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Type number:");
+        System.out.println("Type amount ZL to conver to USD:");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int result = square(number);
+        double result = convertToUsd(number);
         System.out.println("Square:");
         System.out.println(result);
     }
-    public static int square(int num){
-        return num*num;
+    public static double convertToUsd(int num){
+        double ratio =4.04;
+        return num*ratio;
     }
 }
