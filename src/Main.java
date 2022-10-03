e5777aa78eb6802abc7596a48e15ade30baecd82
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 19:42:18 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
-//W pliku Main06.java napisz publiczną metodę checkMaturity, która:
-//przyjmie parametr liczbowy age oznaczający wiek użytkownika,
-//sprawdzi, czy użytkownik jest pełnoletni,
-//zwróci wartość true – jeśli jest pełnoletni,
-//zwróci wartość false – jeśli nie jest.
+//W pliku Main07.java napisz publiczną metodę checkEvenOdd, która:
+//przyjmie parametr liczbowy number,
+//będzie zwracać wartość typu String,
+//zwróci wynik "even", jeśli liczba jest parzysta,
+//zwróci wynik "odd", jeśli liczba jest nieparzysta.
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Age:");
+        System.out.println("Number:");
         Scanner scanner = new Scanner(System.in);
-        int age = scanner.nextInt();
-        boolean result = checkMaturity(age);
-        System.out.println("Mature:");
+        int number = scanner.nextInt();
+        String result = checkEvenOdd(number);
+        System.out.println("Number is:");
         System.out.println(result);
     }
-    public static boolean checkMaturity(int age){
-        if(age>=18){
-            return true;
+    public static String checkEvenOdd(int number){
+        if(number%2 == 0){
+            return "even";
         } else {
-            return false;
+            return "odd";
         }
 
     }
