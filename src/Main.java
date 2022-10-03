bfe8d32b14d6a623005cd165b53b6f32aa58ea5e
Jasiu Imac <jan@kaletka.com.pl>
2022-10-03 22:05:20 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,16 @@
-//stwórz 50-elementową tablicę o nazwie mainTab z kolejnymi liczbami całkowitymi od 0 do 49,
-//wypisz na konsoli po kolei elementy tej tablicy, po dziesięć w każdym wierszu,
-//liczby jednocyfrowe uzupełnij dodatkowym zerem na początku (np. 03).
+//stwórz tablicę o nazwie randNumbers
+//przy użyciu pętli for wypełnij tablicę przez 20 losowych liczb z zakresu 0-100, (do otrzymania losowej wartości użyj sposobu opisanego w snippetach),
+//wypisz w konsoli minimalną wartość z tablicy.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Array:");
-        int[] numbers = new int[50];
-        for(int i=0; i<50; i++){
-
-            numbers[i] = i+1;
-            if(numbers[i]<10){
-
-                System.out.print("0"+numbers[i] + " ");
-            }else{
-                System.out.print(numbers[i] + " ");
-            }
-            if(numbers[i] % 10 == 0){
-                System.out.println("");
-            }
+        int min = 100;
+        int[] randNumbers = new int[20];
+        for(int i=0; i<randNumbers.length;i++){
+            randNumbers[i] = (int)(Math.random()*101);
+            min = Math.min(randNumbers[i],min);
         }
-
+        System.out.println(min);
     }
 
 }
