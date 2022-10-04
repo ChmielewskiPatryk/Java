96423692700c99afccbb2f69874f2f6f623e8ffd
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 16:34:32 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,10 @@ wyświetli na konsoli każdego bohatera w oddzielnej linii.
 */
 public class Main {
     public static void main(String[] args) {
-
-        String str = "Bohater1, Bohater2, Bohater3, Bohater4, Bohater5";
-        String[] splitted = str.split(",");
-        for(String item :splitted){
-            System.out.println(item.trim());
-        }
+        System.out.println(countTokens(" To jest tekst do "));
+    }
+    public static int countTokens(String str){
+        String[] words = str.trim().split(" ");
+        return words.length;
     }
 }
