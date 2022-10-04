4ea77d956a08b8b8fa19d66a49afdce27405c457
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 14:26:28 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
 
 /*
-Palindrom to słowo lub zdanie, które brzmi tak samo zarówno czytane wspak, jak i od początku, np.: "kajak", "radar", "Kobyła ma mały bok".
-W pliku Main07.java napisz program, który sprawdzi, czy zmienna str, umieszczona w klasie, jest palindromem.
-Uwaga: podczas sprawdzania palindromu zignoruj spacje.
+public static String toggleChar(String str) .
+Uzupełnij ciało metody tak, aby zamieniała duże litery na małe i odwrotnie,
+a następnie zwracała wynik.
+Przykład: dla napisu TaK wyświetli tAk.
 */
 public class Main {
     public static void main(String[] args) {
-        String str = "A to kanapa pana kota";
-        String lowerCase = str.toLowerCase();
-        String joinStr = lowerCase.replaceAll("\\s+", "");
-        char[] strArr = joinStr.toCharArray();
-        int length = strArr.length;
-        char[] reverseArr = new char[length];
-        for (int i = 0; i < length; i++) {
-            reverseArr[i] = strArr[length - 1 - i];
+        System.out.println(toggleChar("aAdSeGFDdsssd"));
+    }
+    static String toggleChar(String str) {
+        char[] arr = str.toCharArray();
+        int len = arr.length;
+        String toggledString;
+        for (int i = 0; i < len; i++) {
+            if (Character.isUpperCase(arr[i])) {
+                arr[i] = Character.toLowerCase(arr[i]);
+            } else {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
         }
-        String reversedStr = String.valueOf(reverseArr);
-        System.out.println(joinStr.equals(reversedStr));
+        toggledString = String.valueOf(arr);
+        return toggledString;
     }
 }
