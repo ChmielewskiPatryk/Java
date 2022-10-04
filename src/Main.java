a34c80e55b2beaad7b7455afdb733fed3aa9c50f
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 16:41:48 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 
 /*
-umieść metodę o sygnaturze public static int countTokens(String str),
-uzupełnij ciało metody tak, aby zwracała liczbę wyrazów w podanym ciągu znaków.
-Metoda powinna pomijać białe znaki.
-
-Przykład: dla String str = " To jest tekst do "; mamy otrzymać wartość 4;
+public static String[] onlyTwoElements(String str, char separator).
+Uzupełnij ciało metody tak, aby zwracała tablicę dwóch elementów,
+które powstały po podzieleniu tekstu (str) po pierwszym napotkanym znaku podziału (separator).
+Przykład dla napisu Java-zadania-podzial-napisow oraz separatora - otrzymamy:
+pierwszy element tablicy: Java
+drugi element: zadania-podzial-napisow.
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(countTokens(" To jest tekst do "));
+        System.out.println(onlyTwoElements("Java-zadania-podzial-napisow",'-')[0]);
+        System.out.println(onlyTwoElements("Java-zadania-podzial-napisow",'-')[1]);
     }
-    public static int countTokens(String str){
-        String[] words = str.trim().split(" ");
-        return words.length;
+    public static String[] onlyTwoElements(String str, char separator){
+        String sep = String.valueOf(separator);
+        return str.split(sep,2);
     }
 }
