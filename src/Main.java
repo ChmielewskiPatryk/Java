d3f1905888619860ae800444b61f071087723983
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 13:38:19 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 
 /*
-public static String stringFromArray(String[] str).
-Uzupełnij ciało metody tak,
-aby zwracała napis powstały z połączenia wszystkich napisów występujących w tablicy str.
+W pliku Main06.java napisz program,
+który dla znajdującej się w pliku zmiennej str wyświetli na konsoli odwrócony napis.
+Przykład: dla napisu Tak wyświetli kaT.
 */
 public class Main {
+
     public static void main(String[] args) {
-        String[] stringArray = {"aaaa", "bbbb", "cccc", "dddd"};
-        System.out.println(stringFromArray(stringArray));
-
-    }
-
-    static String stringFromArray(String[] str) {
-        return String.join("", str);
+        String str = "codersLabxyz";
+        String reverseStr;
+        int length = str.length();
+        char[] reverse = new char[length];
+        for (int i = 0; i < length; i++) {
+            reverse[i] = str.charAt(length - 1 - i);
+        }
+        reverseStr = String.valueOf(reverse);
+        System.out.println(reverseStr);
     }
 }
