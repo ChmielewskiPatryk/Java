cdfe50ab49ff86384be934b3376297c40813e63e
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 09:48:47 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
-//W pakiecie pl.coderslab.homeworks.methods, w pliku Main01.java umieść metodę o sygnaturze public static double dogAge(double dogAge) , która przeliczy wiek psa w psich latach.
-//funkcja powinna przyjmować wiek psa jako parametr,
-//dla pierwszych dwóch lat, każdy rok życia psa jest równy 10.5 ludzkiego roku,
-//powyżej dwóch lat, każdy rok życia psa, to 4 ludzkie lata,
-//funkcja powinna zwrócić wiek psa w przeliczeniu na lata ludzkie.
-
-import java.util.Arrays;
-
+/*
+W pakiecie pl.coderslab.homeworks.methods,
+w pliku Main02.java umieść metodę o sygnaturze public static boolean divisibleBy(int a, int b ):
+Uzupełnij ciało metody tak, aby zwracała true bądź false w zależności czy zmienna a jest podzielna przez b.
+*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println(dogAge(1.5));
-        System.out.println(dogAge(5));
+        System.out.println(divisibleBy(10,2));
+        System.out.println(divisibleBy(10,3));
     }
-    public static double dogAge(double dogAge){
-        double realAge;
-        if(dogAge<=2){
-            realAge = dogAge*10.5;
+    public static boolean divisibleBy(int a, int b ){
+        if(a%b == 0){
+            return true;
         }else{
-            realAge = 2*10.5 + (dogAge-2)*4;
+            return false;
         }
-        return  realAge;
     }
 
 }
