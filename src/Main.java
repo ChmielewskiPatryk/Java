a35b4f4ddd12b077faabd669eb4fa018a4d4f353
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 10:04:06 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
+import java.util.Arrays;
+
 /*
-W pakiecie pl.coderslab.homeworks.methods,
-w pliku Main02.java umieść metodę o sygnaturze public static boolean divisibleBy(int a, int b ):
-Uzupełnij ciało metody tak, aby zwracała true bądź false w zależności czy zmienna a jest podzielna przez b.
+Uzupełnij ciało metody tak, aby dla otrzymanej tablicy jej zawartość została skopiowana do tymczasowej tablicy, której rozmiar ma być 2-krotnie większy od tablicy podanej.
+Uzupełnij brakujące elementy tymczasowej tablicy elementami tablicy pierwotnej w odwrotnej kolejności.
+Metoda ma zwrócić skopiowaną i uzupełnioną tablicę. Przykład: dla tablicy int arr[] = {1,2,3}; ma zostać zwrócona tablica int arrTmp[] = {1,2,3,3,2,1};
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(divisibleBy(10,2));
-        System.out.println(divisibleBy(10,3));
-    }
-    public static boolean divisibleBy(int a, int b ){
-        if(a%b == 0){
-            return true;
-        }else{
-            return false;
+        int[] arr = {1, 2, 3};
+        int[] arrResult = append(arr);
+        for(int item:arrResult){
+            System.out.println(item);
         }
+
+    }
+
+    public static int[] append(int[] arr) {
+        int length = arr.length;
+        int[] arr2 = Arrays.copyOf(arr, 2 * length);
+        for (int i=0;i<length;i++){
+            arr2[length+i]=arr[length-i-1];
+        }
+        return arr2;
+
     }
 
 }
