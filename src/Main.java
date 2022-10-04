9f713d06908510ce017a3c943fec240b7814cd1c
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 17:41:51 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,18 @@
 
 /*
-public static int[] lessMore(int[][] arr).
-Uzupełnij ciało metody tak, aby dla otrzymanej tablicy 2-wymiarowej obliczyć średnią arytmetyczną.
-Oblicz ile elementów jest mniejszych a ile większych od średniej.
-Metoda ma zwrócić tablicę składającą się z 2 elementów
-1 - ilość elementów mniejszych od średniej, 2- ilość elementów większych.
+public static String replaceChar(String str, char forReplace, char replacement).
+Uzupełnij ciało metody tak, aby zwracała napis powstały z zamiany w napisie str
+wszystkich znaków forReplace przez znaki replacement.
 */
 public class Main {
     public static void main(String[] args) {
-        int[][] arr = {{1, 4, 7}, {6, 3, 8}, {7, 5, 7, 2}};
-        System.out.println(lessMore(arr)[0]);
-        System.out.println(lessMore(arr)[1]);
+        System.out.println(replaceChar("aaaaaadddaaaa",'a','c'));
+    }
+    public static String replaceChar(String str, char forReplace, char replacement){
+        String replace = String.valueOf(forReplace);
+        String replacemen = String.valueOf(replacement);
+        return  str.replaceAll(replace,replacemen);
     }
 
-    public static int[] lessMore(int[][] arr) {
-        int count = 0;
-        int sum = 0;
-        int[] res = new int[2];
-        int countLess = 0;
-        int countMore = 0;
-        double average;
-
-        for (int[] items : arr) {
-            for (int item : items) {
-                count++;
-                sum += item;
-            }
-        }
-        average = sum / count;
-
-        for (int[] items : arr) {
-            for (int item : items) {
-                if (item > average) {
-                    countMore++;
-                } else if (item < average) {
-                    countLess++;
-                }
-            }
-        }
-
-        res[0] = countLess;
-        res[1] = countMore;
-        return res;
-    }
 
 }
