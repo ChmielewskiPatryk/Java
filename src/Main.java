699d76debf7aaa55b552f4158cc84371b61465ff
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 10:31:54 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
 import java.util.Arrays;
 
 /*
-Uzupełnij ciało metody tak, aby dla otrzymanej tablicy jej zawartość została skopiowana do tymczasowej tablicy, której rozmiar ma być 2-krotnie większy od tablicy podanej.
-Uzupełnij brakujące elementy tymczasowej tablicy elementami tablicy pierwotnej w odwrotnej kolejności.
-Metoda ma zwrócić skopiowaną i uzupełnioną tablicę. Przykład: dla tablicy int arr[] = {1,2,3}; ma zostać zwrócona tablica int arrTmp[] = {1,2,3,3,2,1};
+public static boolean contains(int[] arr, int element).
+uzupełnij ciało metody tak, by sprawdzała, czy w tablicy arr istnieje element,
+jeżeli tak ma zwracać true,
+jeżeli nie ma zwracać false.
 */
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        int[] arrResult = append(arr);
-        for(int item:arrResult){
-            System.out.println(item);
-        }
-
+        int[] arr = {2, 5, 6, 7, 8, 9, 3};
+        System.out.println(contains(arr, 1));
     }
 
-    public static int[] append(int[] arr) {
-        int length = arr.length;
-        int[] arr2 = Arrays.copyOf(arr, 2 * length);
-        for (int i=0;i<length;i++){
-            arr2[length+i]=arr[length-i-1];
+    public static boolean contains(int[] arr, int element) {
+        boolean flag = false;
+        for (int item : arr) {
+            if (item == element) {
+                flag = true;
+            }
         }
-        return arr2;
-
+        return flag;
     }
 
 }
