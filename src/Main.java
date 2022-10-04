0a42d6405e78cd3c6506542810bbcaf3fabe26e6
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 09:45:52 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-//stwórz tablicę o nazwie numbers zawierającą 10 dowolnych liczb,
-//stwórz drugą tablicę, nazwij ją secondNumbers, wypełnij 10 dowolnymi liczbami,
-//stwórz trzecią tablicę zawierającą sumy elementów, które mają w tablicach pozycję o tym samym indeksie.
+//W pakiecie pl.coderslab.homeworks.methods, w pliku Main01.java umieść metodę o sygnaturze public static double dogAge(double dogAge) , która przeliczy wiek psa w psich latach.
+//funkcja powinna przyjmować wiek psa jako parametr,
+//dla pierwszych dwóch lat, każdy rok życia psa jest równy 10.5 ludzkiego roku,
+//powyżej dwóch lat, każdy rok życia psa, to 4 ludzkie lata,
+//funkcja powinna zwrócić wiek psa w przeliczeniu na lata ludzkie.
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2, 5, 7, 3, 5, 2, 8, 6, 4, 9};
-        int[] secondNumbers = {6, 8, 3, 9, 5, 4, 2, 1, 4, 5};
-        int[] thirdNumbers = new int[10];
-        // dziala ale nie wiem czy tu chca jakies Array methods?
-        for (int i =0; i<10;i++){
-            thirdNumbers[i]=numbers[i]+secondNumbers[i];
-            System.out.print(thirdNumbers[i]+",");
-        }
+        System.out.println(dogAge(1.5));
+        System.out.println(dogAge(5));
     }
+    public static double dogAge(double dogAge){
+        double realAge;
+        if(dogAge<=2){
+            realAge = dogAge*10.5;
+        }else{
+            realAge = 2*10.5 + (dogAge-2)*4;
+        }
+        return  realAge;
+    }
+
 }
