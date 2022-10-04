f339ac45627cea9949850d8951651d8d8cf05e4a
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 14:38:42 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 
 /*
-public static String toggleChar(String str) .
-Uzupełnij ciało metody tak, aby zamieniała duże litery na małe i odwrotnie,
-a następnie zwracała wynik.
-Przykład: dla napisu TaK wyświetli tAk.
+podzieli na wyrazy ciąg znaków przechowywany w zmiennej str, znajdującej się w pliku,
+wyświetli na konsoli każdy z tych wyrazów w oddzielnej linii.
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(toggleChar("aAdSeGFDdsssd"));
-    }
-    static String toggleChar(String str) {
-        char[] arr = str.toCharArray();
-        int len = arr.length;
-        String toggledString;
-        for (int i = 0; i < len; i++) {
-            if (Character.isUpperCase(arr[i])) {
-                arr[i] = Character.toLowerCase(arr[i]);
-            } else {
-                arr[i] = Character.toUpperCase(arr[i]);
-            }
+        String str = "Naucz się programować od podstaw i rozwiń wymarzoną karierę w branży IT.";
+        String[] strSplitted = str.split(" ");
+        for(String stri : strSplitted){
+            System.out.println(stri);
         }
-        toggledString = String.valueOf(arr);
-        return toggledString;
     }
 }
