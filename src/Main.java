c686b51c974b7bf5cf095898ffebeb4c42c2386b
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 11:17:48 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import java.util.Arrays;
 
 /*
-1. wypisz element znajdujący się w 3 kolumnie i 2 wierszu,
-oczekiwana wartość to 12
-2. wypisz długość tablicy znajdującej się w drugim wierszu,
-oczekiwana wartość to 4
-3.wypisz element znajdujący się w 4 kolumnie i 3 wierszu.
-oczekiwana wartość to 4
+1. wypisz długości tablic jednowymiarowych, z których składa się tablica dwuwymiarowa,
+2. wypisz wszystkie elementy tablicy task2Array (musisz użyć do tego zagnieżdżonej pętli for).
 */
 public class Main {
     public static void main(String[] args) {
-        int[][] task1Array = {{2, 3, 4}, {12, 32, 12, 11}, {3, 2, 1, 4, 5}, {5, 1, 6, 7, 8}};
-        System.out.println(task1Array[1][2]); //1
-        System.out.println(task1Array[1].length); //2
-        System.out.println(task1Array[2][3]); //3
+        int[][] task2Array = {{2, 3, 4}, {12, 32, 12, 11}, {3, 2, 1, 4, 5}, {5, 1, 6, 7, 8}};
+        //1
+        for (int[] item : task2Array) {
+            System.out.println(item.length);
+        }
+        //2
+        for (int[] item : task2Array) {
+            for (int item2 : item) {
+                System.out.print(item2 +",");
+            }
+        }
+
     }
 
 }
