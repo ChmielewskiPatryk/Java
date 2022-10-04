caec80a25617a424309bb44c77d6615fab909ce9
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 18:02:02 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 
 /*
-public static String replaceChar(String str, char forReplace, char replacement).
-Uzupełnij ciało metody tak, aby zwracała napis powstały z zamiany w napisie str
-wszystkich znaków forReplace przez znaki replacement.
+public static String upperCase(String str, int index) .
+Metoda zwraca napis str, w którym znaki zostały zamienione
+na duże z pozycji podzielnych przez index.
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(replaceStr("aa bb dd ee", "aa", "cc"));
+        System.out.println(upperCase("którym znaki zostały zamienione na duże z pozycji", 3));
     }
 
-    public static String replaceStr(String str, String forReplace, String replacement) {
-        return str.replaceAll(forReplace, replacement);
-    }
+    public static String upperCase(String str, int index) {
+        char[] arr = str.toCharArray();
 
+        for (int i = 0; i < arr.length; i++) {
+            if (i % index == 0) {
+                arr[i] = Character.toUpperCase(arr[i]);
+            }
+        }
+        return String.valueOf(arr);
+
+    }
 
 }
