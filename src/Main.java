b26cc640f856052f5fcd56e0fde0c09a97fb5e4b
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 12:17:19 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 
 /*
-1. pobierze przedostatnią literę zmiennej str, znajdującej się w pliku,
-2. wyświetli na konsoli liczbę wystąpień tej litery w całym ciągu znaków
+public static int charPos(String str, char c) .
+Uzupełnij ciało metody tak, aby zwracała wartość pierwszego indeksu,
+pod którym w napisie str znajduje się znak c.
 */
 public class Main {
-    public static void main(String[] args) {
-        String str = "CodersLab jak dobrze zacząć programować";
-        int strLength = str.length();
-        char getChar = str.charAt(strLength - 2);
-        int count = 0;
-        for (int i = 0; i < strLength - 1; i++) {
-            if (str.charAt(i) == getChar) {
-                count++;
-            }
-        }
-        System.out.println(getChar);
-        System.out.println(count);
 
+    public static void main(String[] args) {
+        System.out.println(charPos("Dupa", 'p'));
+
+    }
+
+    static int charPos(String str, char c) {
+        return str.indexOf(c);
     }
 }
