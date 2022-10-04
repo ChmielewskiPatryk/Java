a8ef1b6f4dc364b676c7eca8bfc0e487c71d5b33
Jasiu Imac <jan@kaletka.com.pl>
2022-10-04 17:28:15 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,47 @@
 
 /*
-Uzupełnij ciało metody tak, aby dla otrzymanej tablicy 2-wymiarowej
-dla każdego jej wiersza wyznaczyła minimalną wartość.
-Poszczególne minimalne wartości mają być umieszczone w tablicy, którą metoda zwraca.
+public static int[] lessMore(int[][] arr).
+Uzupełnij ciało metody tak, aby dla otrzymanej tablicy 2-wymiarowej obliczyć średnią arytmetyczną.
+Oblicz ile elementów jest mniejszych a ile większych od średniej.
+Metoda ma zwrócić tablicę składającą się z 2 elementów
+1 - ilość elementów mniejszych od średniej, 2- ilość elementów większych.
 */
 public class Main {
     public static void main(String[] args) {
-        int arr[][] = {
-                {2, 3, 6, 1},
-                {4, 5, 6, 7}
-        };
-        System.out.println(minimum(arr)[0]);
-        System.out.println(minimum(arr)[1]);
+        int[][] arr = {{1, 4, 7}, {6, 3, 8}, {7, 5, 7, 2}};
+        System.out.println(lessMore(arr)[0]);
+        System.out.println(lessMore(arr)[1]);
     }
 
-    public static int[] minimum(int[][] arr) {
-        int[] minValArr = new int[arr.length];
+    public static int[] lessMore(int[][] arr) {
+        int count = 0;
+        int sum = 0;
+        int[] res = new int[2];
+        int countLess = 0;
+        int countMore = 0;
+        double average;
 
-        for (int i = 0; i < arr.length; i++) {
-            int minVal = arr[i][0];
+        for (int[] items : arr) {
+            for (int item : items) {
+                count++;
+                sum += item;
+            }
+        }
+        average = sum / count;
 
-            for (int j = 0; j < arr[i].length; j++) {
-                if (minVal > arr[i][j]) {
-                    minVal = arr[i][j];
+        for (int[] items : arr) {
+            for (int item : items) {
+                if (item > average) {
+                    countMore++;
+                } else if (item < average) {
+                    countLess++;
                 }
             }
-            minValArr[i] = minVal;
         }
 
-        return minValArr;
+        res[0] = countLess;
+        res[1] = countMore;
+        return res;
     }
+
 }
