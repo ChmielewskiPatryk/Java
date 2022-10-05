68de47d326a4e76b281a487d1806936efc2555a2
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 10:10:11 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 
 /*
-ustaw breakpoint w miejscu instrukcji int x = 33 * 55; a następnie uruchom program w trybie debugowania,
-zrób krok do przodu i zobacz zmienną x,
-zrób krok do przodu i zobacz ponownie zmienną text,
-zrób krok do przodu i przejdź do metody bar z klasy Foo przy pomocy przycisku Step into,
-wróć do metody main klasy Main01 przy pomocy przycisku Step out,
-puść program do końca przy pomocy przycisku Resume.
+ustaw breakpoint w miejscu instrukcji int a = 11 / 3; a następnie uruchom program w trybie debugowania,
+zrób krok do przodu aż do końca i zaobserwuj wartości, które przyjmują znajdujące się tam zmienne.
 */
 public class Main {
     public static void main(String[] args) {
-        int x = 33 * 55;
-        String text = "abcd" + " " + x;
-        Foo.bar();
-        boolean isTrue = !false;
+        int a = 11 / 3;
 
-        char c = 'x';
+        int max = Integer.MAX_VALUE;
+
+        int min = Integer.MIN_VALUE;
+
+        int x = max + 1;
+
+        char c = (char)55;
+
+        boolean d = !!true;
 
     }
 }
