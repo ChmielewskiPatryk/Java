db592d45425cf60d109ed7e59f690c984d2bec91
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 09:22:31 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,53 @@ Uzupełnij ciało metody tak, by zwracała napis zaszyfrowany szyfrem Cezara.
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(encode("Dupa"));
+        String encoded = encode("DupaZz", 2);
+        System.out.println(encoded);
+        String decoded = decode(encoded, 2);
+        System.out.println(decoded);
+
     }
 
-    public static String encode(String str) {
-        StringBuilder result = new StringBuilder();
+    public static String decode(String str, int shift) {
+        StringBuilder res = new StringBuilder();
 
         for (int i = 0; i < str.length(); i++) {
-            if (Character.isUpperCase(str.charAt(i)))
-            {
-                char ch = (char)(((int)str.charAt(i) +
-                        1 - 65) % 26 + 65);
-                result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)str.charAt(i) +
-                        1 - 97) % 26 + 97);
-                result.append(ch);
+            if (Character.isUpperCase(str.charAt(i))) {
+                int uni = ((int) str.charAt(i) - shift - 65);
+                if (uni < 0) {
+                    uni = uni + 26;
+                }
+                char ch = (char) (uni % 26 + 65);
+                res.append(ch);
+            } else {
+                int uni = ((int) str.charAt(i) - shift - 97);
+                if (uni < 0) {
+                    uni = uni + 26;
+                }
+                char ch = (char) (uni % 26 + 97);
+                res.append(ch);
             }
         }
-        String stringRes = result.toString();
+        String stringRes = res.toString();
+
+        return stringRes;
+
+    }
+
+
+    public static String encode(String str, int shift) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                char ch = (char) (((int) str.charAt(i) + shift - 65) % 26 + 65);
+                res.append(ch);
+            } else {
+                char ch = (char) (((int) str.charAt(i) + shift - 97) % 26 + 97);
+                res.append(ch);
+            }
+        }
+        String stringRes = res.toString();
 
         return stringRes;
     }
