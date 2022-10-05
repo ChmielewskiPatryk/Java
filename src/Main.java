b842ea0b154285ec992b6e427359303b6cdb73bd
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 16:44:00 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 
 /*
 uruchom program i zaobserwuj jaki wyjątek został rzucony,
-dodaj do kodu obsługę odpowiedniego wyjątku i uruchom ponownie program,
-zwróć uwagę czy jest to wyjątek kontrolowany, czy niekontrolowany i dlaczego.
+zmodyfikuj kod tak, aby zabezpieczyć go przed rzuceniem wyjątku,
+(zmodyfikuj kod tak, by w tym miejscu ten wyjątek nie był rzucany).
 */
 public class Main {
     public static void main(String[] args) {
-        int[] tab = {  5, 1, 6, 7, 8 } ;
-        try {
-            System.out.println(tab[21]);
-        } catch (ArrayIndexOutOfBoundsException e){
-            //wyjatek niekontrolowalny bo nie trzeba go obslugiwac ale mozna
-            System.out.println("Table is not so long ");
-        }
-
+        showLength(null);
     }
+
+    static void showLength(String s) {
+        if (s != null) {
+            System.out.println(s.length());
+        }
+    }
+
 }
+
