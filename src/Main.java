ea1e9f30d8471bee325f9742240e33d90bc436c1
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 10:18:06 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 
 /*
-ustaw breakpoint w linii:
- b = i < 9995 ? a / i : a * i;
-następnie uruchom program w trybie debugowania.
-wznawiaj program kilka razy, a następnie go zatrzymaj,
-zmodyfikuj breakpoint nadając mu warunek i<3,
-debuguj ponownie program, do momentu wystąpienia wyjątku,
-pozostaw program z błędem.
+zdebuguj, a następnie popraw program tak by zwracał poprawny wynik.
+
 */
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        for (int i = 1000; i >= 0; i--) {
-            b = i < 9995 ? a / i : a * i;
-        }
-        System.out.println(b);
+        double sum = calculateSum(new String[]{"0", "1", "1", "2", "3", "5", "8"});
+        System.out.println("Sum " + sum);
+    }
 
+    private static double calculateSum(String[] input) {
+        double result = 0;
+        for (int i = 0; i < input.length; i++) {
+            result += Integer.parseInt(input[i]);
+        }
+        return result;
     }
 }
