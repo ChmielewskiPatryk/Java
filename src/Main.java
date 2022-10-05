07b08d2e60532d87e055abac16926df13c6e13c8
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 08:58:02 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
 
 /*
-public static int tripple(String str)
-
-Metoda zwraca ilość potrójnych wystąpień znaków w napisie
-Przykład dla qazbbbwsx -> 1, dla aaawsxbbb ->2
+public static String encode(String str).
+Uzupełnij ciało metody tak, by zwracała napis zaszyfrowany szyfrem Cezara.
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(tripple("aaawsxbbb"));
-        System.out.println(tripple("qazbbbwsx"));
+        System.out.println(encode("Dupa"));
     }
 
-    public static int tripple(String str) {
-        int len = str.length();
-        int count = 0;
+    public static String encode(String str) {
+        StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < len - 2; i++) {
-            if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i + 1) == str.charAt(i + 2)) {
-                count++;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i)))
+            {
+                char ch = (char)(((int)str.charAt(i) +
+                        1 - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)str.charAt(i) +
+                        1 - 97) % 26 + 97);
+                result.append(ch);
             }
         }
-        return count;
+        String stringRes = result.toString();
+
+        return stringRes;
     }
 
 
