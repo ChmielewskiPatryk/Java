96085e54a6547d511bf4d049ed623daeb3e1c312
Jasiu Imac <jan@kaletka.com.pl>
2022-10-05 10:22:30 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 
 /*
 zdebuguj, a następnie popraw program tak by zwracał poprawny wynik.
-
 */
 public class Main {
     public static void main(String[] args) {
-        double sum = calculateSum(new String[]{"0", "1", "1", "2", "3", "5", "8"});
-        System.out.println("Sum " + sum);
+        double avg = calculateAvg(new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97});
+        System.out.println("AVG " + avg);
     }
 
-    private static double calculateSum(String[] input) {
+    private static double calculateAvg(int[] input) {
         double result = 0;
-        for (int i = 0; i < input.length; i++) {
-            result += Integer.parseInt(input[i]);
+        for (int element : input) {
+            result += element;
         }
-        return result;
+        return result/input.length;
     }
 }
